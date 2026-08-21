ec104401f54ae25ee62f643c6e3c152e773a4825
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-08-21 12:11:52 +0000

    arrasamos

--- components/ItemLista/estilo.js
+++ components/ItemLista/estilo.js
@@ -1,19 +1,24 @@
 import { StyleSheet } from 'react-native';
 
 const estilo = StyleSheet.create({
+
+  nomeFilme: {
+    fontSize: 18,
+    color: '#204550',
+    fontWeight: '600',
+     textAlign: 'center',
+    marginBottom: 5
+   
+    
+  },
   boxFilme:{
     padding: 10,
     borderWidth: 1,
     borderColor: '#000',
     borderRadius: 5,
     marginBottom: 10
-  },
-  nomeFilme: {
-    fontSize: 18,
-    color: '#204550',
-    fontWeight: '600',
-    marginBottom: 5
   }
+  
 });
 
 export default estilo;
--- components/ItemLista/index.js
+++ components/ItemLista/index.js
@@ -5,11 +5,15 @@ import estilo from './estilo';
 export default function ItemLista(props) {
   return (
     <View style={estilo.boxFilme}>
-      <Image source={props.imagem} style={{ width: 100, height: 100 }} />
-      <Text style={estilo.nomeFilme}>
+        <Text style={estilo.nomeFilme}>
         {props.titulo}
       </Text>
-
+      <Image source={props.imagem} style={{ width: 200, height: 200, alignSelf: 'center' }} />
+    
+      
+      <Text>
+        {props.descricao}
+      </Text>
       <Text>
         Estreia: {props.Estreia}
       </Text>
