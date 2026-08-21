f4628ddaf2e1b3c2d2a0338ce9cb9600726b2746
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-08-21 11:50:36 +0000

    feito:))

--- components/ItemLista/index.js
+++ components/ItemLista/index.js
@@ -1,21 +1,3 @@
- //import React from 'react';
- //import { View, Text, Image } from 'react-native';
- //import estilo from './estilo';
-
- //export default function ItemLista(props) {
- //  return (
- //    <View style={estilo.boxFilme}>
- //      <Image source={props.imagem} style={{width:60, height:60 }}/>
- //      <Text style={estilo.nomeFilme}>
- //        {props.titulo}
- //      </Text>
- //      
- //      <Text>
- //        Estreia: {props.estreia}
- //      </Text>
- //    </View>
- //  )
- //}
  import React from 'react';
 import { View, Text, Image } from 'react-native';
 import estilo from './estilo';
@@ -23,7 +5,7 @@ import estilo from './estilo';
 export default function ItemLista(props) {
   return (
     <View style={estilo.boxFilme}>
-      <Image source={props.imagem} style={{ width: 60, height: 60 }} />
+      <Image source={props.imagem} style={{ width: 100, height: 100 }} />
       <Text style={estilo.nomeFilme}>
         {props.titulo}
       </Text>
