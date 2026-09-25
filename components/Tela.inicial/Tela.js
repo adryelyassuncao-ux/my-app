3d406bb5582db83a3a24d1d7d9d92eea6f1e5dbe
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-09-25 12:13:40 +0000

    lala

--- components/Tela.inicial/Tela.js
+++ components/Tela.inicial/Tela.js
@@ -4,43 +4,37 @@ import estilo from './css'
 
 const Tela = () => {
 
- const [num_escolhido, setNumEscolhido] = useState('');
- const [num_sorteado, setNumSorteado] = useState(0);
- const [num_rodadas, setNumRodadas] = useState(1);
- const [textoguardado, setTextoGuardado] = useState('');
- const [pontuacao, setRodadaPontuacao] = useState(0);
- const [jogoFinalizado, setJogoFinalizado] = useState(false);
- const [pontoRodada, setPontoRodada] = useState(0);
+  const [num_escolhido, setNumEscolhido] = useState('');
+  const [num_sorteado, setNumSorteado] = useState(0);
+  const [num_rodadas, setNumRodadas] = useState(1);
+  const [textoguardado, setTextoGuardado] = useState('');
+  const [pontuacao, setRodadaPontuacao] = useState(0);
+  const [jogoFinalizado, setJogoFinalizado] = useState(false);
+  const [pontoRodada, setPontoRodada] = useState(0);
 
 
 
-const ValidarNum = () =>
- {
-  const num = parseInt(num_escolhido,10)
-  if(num >= 100 || num < 0 || isNaN(num))
-  {
-    setTextoGuardado('O número deve ser entre 0 a 99');
-    return false;
-  }
-  else
-  {
-    setTextoGuardado('');
-    return true;
-  }
+  const ValidarNum = () => {
+    const num = parseInt(num_escolhido, 10)
+    if (num >= 100 || num < 0 || isNaN(num)) {
+      setTextoGuardado('O número deve ser entre 0 a 99');
+      return false;
+    }
+    else {
+      setTextoGuardado('');
+      return true;
+    }
 
   };
 
- const criarNumero = () =>
- {
-    if(!ValidarNum())
-    {return; }
+  const criarNumero = () => {
+    if (!ValidarNum()) { return; }
 
-    if(num_rodadas <= 5)
-    {
+    if (num_rodadas <= 5) {
       const NumeroCriado = Math.floor(Math.random() * 100);
-      setNumSorteado (NumeroCriado);
-      
-      const num = parseInt(num_escolhido,10)
+      setNumSorteado(NumeroCriado);
+
+      const num = parseInt(num_escolhido, 10)
 
       const operacao = num - NumeroCriado;
       const Ponto = Math.abs(operacao);
@@ -48,61 +42,60 @@ const ValidarNum = () =>
 
       setPontoRodada(resultado);
 
-    setRodadaPontuacao(prevPontos => prevPontos + resultado);
-    setNumRodadas(prevRodadas => prevRodadas + 1);
-    setNumEscolhido ('');
+      setRodadaPontuacao(prevPontos => prevPontos + resultado);
+      setNumRodadas(prevRodadas => prevRodadas + 1);
+      setNumEscolhido('');
     }
 
     if (num_rodadas === 5) {
       setJogoFinalizado(true);
     }
 
- };
+  };
   const MensagemPontuacao = (pontos) => {
-  if (pontos >= 450 && pontos <= 499) {
-    return 'Quase perfeito!';
-  } else if (pontos >= 300 && pontos <= 449) {
-    return 'Mandou bem!';
-  } else if (pontos >= 100 && pontos <= 299) {
-    return 'Está quase lá!';
-  } else if (pontos === 500) {
-    return 'Pontuação máxima! Incrível!';
-  } else {
-    return '';
-  }
-}; 
+    if(pontos == 500)
+      return 'Pontuação máxima! Incrível!';
+    if(pontos > 449)
+      return 'Quase perfeito!'; 
+    if(pontos > 299)
+      return 'Mandou bem!';
+    if (pontos > 249) 
+      return 'Está quase lá!';
+    
+    return 'Muito ruim';
+  };
 
-const reiniciarJogo = () => {
-  setNumEscolhido('');
-  setNumSorteado(0);
-  setNumRodadas(1);
-  setTextoGuardado('');
-  setRodadaPontuacao(0);   
-  setPontoRodada(0);   
-};
+  const reiniciarJogo = () => {
+    setNumEscolhido('');
+    setNumSorteado(0);
+    setNumRodadas(1);
+    setTextoGuardado('');
+    setRodadaPontuacao(0);
+    setPontoRodada(0);
+  };
 
   return (
 
     <ScrollView contentContainerStyle={estilo.Container}>
-     <Text style={estilo.Titulo}>Jogo do Sorteio</Text>
-     
+      <Text style={estilo.Titulo}>Jogo do Sorteio</Text>
+
 
       <View style={estilo.Bola}>
-       <Text style={estilo.textoBola}>{num_sorteado}</Text>
-       <Text>Rodada: {num_rodadas - 1}</Text>
+        <Text style={estilo.textoBola}>{num_sorteado}</Text>
+        <Text>Rodada: {num_rodadas - 1}</Text>
 
       </View>
-    
+
       <Text style={estilo.texto}>{textoguardado}</Text>
       <Text>Pontuação da rodada: {pontoRodada}</Text>
 
-      {jogoFinalizado &&(
-      <View style={estilo.Caixa}>
+      {jogoFinalizado && (
+        <View style={estilo.Caixa}>
           <Text>{MensagemPontuacao(pontuacao)}</Text>
           <Text style={estilo.TextoPontuacao}>Pontuação Total: {pontuacao}</Text>
-       
-    </View>
-    )}
+
+        </View>
+      )}
       <TextInput
         placeholder="Digite um numero entre 0 a 99 "
         keyboardType="numeric"
@@ -112,21 +105,21 @@ const reiniciarJogo = () => {
       />
 
       <View style={estilo.Botao}>
-      <Button
-      title="Sorteiar"
-      onPress={criarNumero}
-      />
+        <Button
+          title="Sorteiar"
+          onPress={criarNumero}
+        />
       </View>
-        <View style={estilo.Reiniciar}>
-            <Button 
-          title="Jogar Novamente" 
-          onPress={reiniciarJogo} 
+      <View style={estilo.Reiniciar}>
+        <Button
+          title="Jogar Novamente"
+          onPress={reiniciarJogo}
         />
       </View>
     </ScrollView>
-  
+
 
   );
- };
+};
 
 export default Tela;
